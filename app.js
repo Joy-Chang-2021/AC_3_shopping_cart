503fc610cac91d80618e24f468c3e424c9212eb6
Joy-Chang-2021 <joy50422@gmail.com>
2022-01-06 22:21:45 +0800

    initial project

---
+++ app.js
@@ -0,0 +1,17 @@
+// Include express from node_modules and define server related variables
+const express = require('express')
+const app = express()
+const port = 3000
+
+// static file
+app.use(express.static('public'))
+
+// setting the route and corresponding response
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/shopping-cart.html')
+})
+
+// Listen the server when it started
+app.listen(port, () => {
+  console.log(`Express is running on http://localhost:${port}`)
+})
