a828b6e57f8db3d96b5001a47267571fecd602b2
Joy-Chang-2021 <joy50422@gmail.com>
2022-01-09 01:06:52 +0800

    main / shopping cart / JS

--- public/javascript/cart.js
+++ public/javascript/cart.js
@@ -1,4 +1,4 @@
-// /////////////////購物車變數/////////////////
+// /////////////////結帳區變數/////////////////
 // 選取按鈕面板*2、按鈕*4
 const stepperControllers = document.querySelectorAll('.stepper-control')
 const preBtns = document.querySelectorAll('.btn-previous')
@@ -8,12 +8,30 @@ const steps = document.querySelectorAll('.step')
 const formParts = document.querySelectorAll('.part')
 //狀態紀錄
 let step = 0
+// /////////////////購物籃變數/////////////////
+const itemsPanel = document.querySelector('.shopping-items')
+const totalCost = document.querySelector('.total-cost').children[1]
+const itemList = [
+  {
+    id: 1,
+    title: "破壞補丁修身牛仔褲",
+    quantity: 1,
+    price: 3999
+  },
+  {
+    id: 2,
+    title: "刷色直筒牛仔褲",
+    quantity: 1,
+    price: 1299
+  }
+]
+
 // /////////////////導覽列變數/////////////////
 const navBtn = document.querySelector('.nav-btn')
 const navbar = document.querySelector('.nav-group')
 const main = document.querySelector('main')
 
-// /////////////////購物車步驟函式/////////////////
+// /////////////////結帳區步驟函式/////////////////
 function stepperControlClicked(event) {
   event.preventDefault()
   const target = event.target
@@ -67,7 +85,48 @@ function stepBtnControll(step) {
     else btn.children[0].innerText = '下一步'
   })
 }
+// /////////////////購物籃變數/////////////////
+function itemsPanelClicked(event) {
+  if (!event.target.matches('.minus') && !event.target.matches('.plus')) return
+  const itemId = event.target.parentElement.dataset.id //取出id
+  const index = itemList.findIndex(item => item.id === Number(itemId)) //number
+  
+  itemListHandler(event, index)
+  itemsPanelRendered(event, index, itemList)
+  totalCostRendered(itemList)
 
+}
+// 修改後台(itemList)儲存之購買數量
+function itemListHandler(event, index) {
+  if (event.target.matches('.minus')) {
+    if (itemList[index].quantity === 0) return
+    itemList[index].quantity -= 1
+  } else if (event.target.matches('.plus')) {
+    itemList[index].quantity += 1
+  }
+}
+// 單一項目：渲染畫面
+function itemsPanelRendered(event, index, list) {
+  //數量
+  const quantity = event.target.parentElement.children[1]
+  quantity.innerText = list[index].quantity
+  //價格
+  const itemCost = event.target.parentElement.nextElementSibling
+  let itemCostHTML = '$'
+  itemCostHTML += list[index].quantity * list[index].price
+  itemCost.innerText = itemCostHTML
+}
+// 總價：渲染畫面
+function totalCostRendered(list) {
+  let totalCostHTML = 0
+  list.forEach(item => {
+    totalCostHTML += item.quantity * item.price
+  })
+  totalCost.innerText = '$' + totalCostHTML
+}
+
+
+totalCost
 // /////////////////導覽列函式/////////////////
 function navBtnClicked(event) {
   navbar.classList.toggle('d-none')
@@ -78,5 +137,7 @@ function navBtnClicked(event) {
 stepperControllers.forEach(controller => {
   controller.addEventListener('click', stepperControlClicked)
 })
+// 購物籃
+itemsPanel.addEventListener('click', itemsPanelClicked)
 // navbar
 navBtn.addEventListener('click', navBtnClicked)
